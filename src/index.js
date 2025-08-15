e6c20e9c3a7ef5914c111cc26d8b9545a5fd25ca
Ruslan.Sokolov <vaquero1master@gmail.com>
2025-08-15 05:11:01 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import "dotenv/config";
 
 import connectDatabase from "./db/connectDatabase.js";
 import startServer from "./server.js";
+import startWebsocketServer from "./wsServer.js";
 
 const bootstrap = async () => {
     await connectDatabase();
     startServer()
+    startWebsocketServer();
 }
 
 bootstrap();
